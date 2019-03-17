c66ea314903fb692050a9fd6fa0ae2d53049ac84
LizunRoman <lizun.roma29@gmail.com>
2019-03-17 15:04:48 +0200

    Updates

---
+++ precache-manifest.5f436829804c19482c05917234a01b4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dc5d3516a29a68b6a59",
+    "url": "/RomanLizun/react/static/js/runtime~main.7dc5d351.js"
+  },
+  {
+    "revision": "d1d253c4b4cc2418a6cd",
+    "url": "/RomanLizun/react/static/js/main.d1d253c4.chunk.js"
+  },
+  {
+    "revision": "77af38c18fc4959e78e0",
+    "url": "/RomanLizun/react/static/js/1.77af38c1.chunk.js"
+  },
+  {
+    "revision": "d1d253c4b4cc2418a6cd",
+    "url": "/RomanLizun/react/static/css/main.4da2b66e.chunk.css"
+  },
+  {
+    "revision": "f644d202f7435c8c41f8a0af90049e32",
+    "url": "/RomanLizun/react/index.html"
+  }
+];
